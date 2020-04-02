209b6694ff37cb9288470a2b747df45eb8825f32
Crow <devcrowmaster@gmail.com>
2020-04-01 22:43:15 -0500

    Initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let form = document.querySelector('.form');
+
+let email = document.querySelector('.form__email');
+let success = document.querySelector('.error-valid');
+
+form.addEventListener('submit', e =>{
+  e.preventDefault();
+
+  if(!validateEmail(email.value)){
+    form.classList.add('error');
+  }else{
+    form.classList.remove('error');
+    success.classList.remove('error-valid');
+    success.classList.add('success');
+    success.innerHTML = 'Thanks you';
+  }
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
